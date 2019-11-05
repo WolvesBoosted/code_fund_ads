6287bb38f0f662375de6eeb9ec37772de48e9183
Andrew Mason <andrewmcodes@protonmail.com>
2019-11-05 18:28:03 -0500

    tests

--- test/models/organization_user_test.rb
+++ test/models/organization_user_test.rb
@@ -13,7 +13,38 @@
 require "test_helper"
 
 class OrganizationUserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @organization_user = organization_users(:member)
+  end
+  test "owners scope" do
+    assert_equal [organization_users(:owner)], OrganizationUser.owners
+  end
+
+  test "administrators scope" do
+    assert_equal [organization_users(:administrator)], OrganizationUser.administrators
+  end
+
+  test "members scope" do
+    assert_equal [@organization_user], OrganizationUser.members
+  end
+
+  test "organization_id uniqueness validation" do
+    ou = OrganizationUser.new(
+      organization: @organization_user.organization,
+      user: @organization_user.user,
+      role: @organization_user.role,
+    )
+    assert_not ou.save
+    assert_includes ou.errors.messages[:organization_id].to_s, "already been taken"
+  end
+
+  test "role inclusion validation" do
+    ou = OrganizationUser.new(
+      organization: @organization_user.organization,
+      user: @organization_user.user,
+      role: "foobar",
+    )
+    assert_not ou.save
+    assert_includes ou.errors.messages[:role].to_s, "not included in the list"
+  end
 end
